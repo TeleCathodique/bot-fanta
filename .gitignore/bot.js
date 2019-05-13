7762c545de175b5003c4c32644768fcbc7796044
TeleCathodique <49343457+TeleCathodique@users.noreply.github.com>
2019-05-13 21:38:17 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 });
 
-var sons = ['007','fanta','aieaieouille','navarro']
+var sons = ['007','fanta','aieaieouille','navarro','foutre']
 
 
 client.on('message', function (message) {
