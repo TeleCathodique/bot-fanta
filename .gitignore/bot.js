c661021e33b8d008989e9c074bbd9875a69a420b
TeleCathodique <49343457+TeleCathodique@users.noreply.github.com>
2020-02-01 21:42:36 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 });
 
-var sons = ['007','fanta','aieaieouille','navarro','foutre','honteux','sel','merde','pan','mauvais','putain','karaba','fun','dansledos']
+var sons = ['007','fanta','aieaieouille','navarro','foutre','honteux','sel','merde','pan','mauvais','putain','karaba','fun','dansledos','chialer']
 
 
 client.on('message', function (message) {
