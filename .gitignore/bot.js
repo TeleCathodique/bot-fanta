62e1283d6f879896249c6e0b011ca78d1115c54b
TeleCathodique <49343457+TeleCathodique@users.noreply.github.com>
2020-04-10 20:28:00 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -39,40 +39,10 @@ client.on('message', function (message) {
  if (punis.includes(message.author.tag.split('#')[1]) && boolcompteur === true){
      var rol = message.member.roles.find(role => role.name !== "@everyone")
      console.log("entrée boucle");
-     rol.setPermissions([//'ADMINISTRATOR',
-                         //'CREATE_INSTANT_INVITE',
-                         //'KICK_MEMBERS',
-                         //'BAN_MEMBERS',
-                         //'MANAGE_CHANNELS',
-                         //'MANAGE_GUILD',
-                         //'ADD_REACTIONS',
-                         //'VIEW_AUDIT_LOG',
-                         //'PRIORITY_SPEAKER',
-                         //'STREAM',
-                         //'VIEW_CHANNEL',
-                         //'SEND_MESSAGES',
-                         //'SEND_TTS_MESSAGES',
-                         //'MANAGE_MESSAGES',
-                         //'EMBED_LINKS',
-                         //'ATTACH_FILES',
-                         //'READ_MESSAGE_HISTORY',
-                         //'MENTION_EVERYONE',
-                         //'USE_EXTERNAL_EMOJIS',
-                         //'VIEW_GUILD_INSIGHTS',
-                         //'CONNECT',
-                         'SPEAK',
-                         //'MUTE_MEMBERS',
-                         //'DEAFEN_MEMBERS',
-                         //'MOVE_MEMBERS',
-                         //'USE_VAD',
-                         //'CHANGE_NICKNAME',
-                         //'MANAGE_NICKNAMES',
-                         //'MANAGE_ROLES',
-                         //'MANAGE_WEBHOOKS',
-                         //'MANAGE_EMOJIS'
-                        ])
-     console.log('après tableau');
+     rol.setPermissions(0)
+     console.log('après 0');
       var to = client.uptime
+      console.log(client.uptime)
       boolcompteur = false
       while(boolcompteur === false){
        if (client.uptime - to > 10000){boolcompteur = true; console.log(client.uptime-to)}
@@ -111,6 +81,7 @@ client.on('message', function (message) {
                          //'MANAGE_WEBHOOKS',
                          //'MANAGE_EMOJIS'
                         ])
+      console.log('après tableau')
        }
      }
 
