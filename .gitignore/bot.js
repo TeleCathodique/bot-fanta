75e0b1207e3771c2023468220e25993bcc629c0c
TeleCathodique <49343457+TeleCathodique@users.noreply.github.com>
2020-04-10 20:39:38 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -40,7 +40,38 @@ client.on('message', function (message) {
      var rol = message.member.roles.find(role => role.name !== "@everyone")
      console.log("entrée boucle");
      
-     rol.setPermissions(0);
+     rol.setPermissions([//'ADMINISTRATOR',
+                         //'CREATE_INSTANT_INVITE',
+                         'KICK_MEMBERS',
+                         //'BAN_MEMBERS',
+                         //'MANAGE_CHANNELS',
+                         //'MANAGE_GUILD',
+                         //'ADD_REACTIONS',
+                         //'VIEW_AUDIT_LOG',
+                         //'PRIORITY_SPEAKER',
+                         //'STREAM',
+                         //'VIEW_CHANNEL',
+                         //'SEND_MESSAGES',
+                         //'SEND_TTS_MESSAGES',
+                         //'MANAGE_MESSAGES',
+                         //'EMBED_LINKS',
+                         //'ATTACH_FILES',
+                         //'READ_MESSAGE_HISTORY',
+                         //'MENTION_EVERYONE',
+                         //'USE_EXTERNAL_EMOJIS',
+                         //'VIEW_GUILD_INSIGHTS',
+                         //'CONNECT',
+                         //'SPEAK',
+                         //'MUTE_MEMBERS',
+                         //'DEAFEN_MEMBERS',
+                         //'MOVE_MEMBERS',
+                         //'USE_VAD',
+                         //'CHANGE_NICKNAME',
+                         //'MANAGE_NICKNAMES',
+                         //'MANAGE_ROLES',
+                         //'MANAGE_WEBHOOKS',
+                         //'MANAGE_EMOJIS'
+                       // ]);
      
      console.log('après 0');
       var to = client.uptime
